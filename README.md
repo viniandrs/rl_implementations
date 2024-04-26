cb1606585206e23e66518b24625283c10dc0ac45
Vinicius <viniciusandreossi@gmail.com>
2024-04-26 14:25:25 -0300

    start debugging deep q-learning

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rl_practice_eldorado
 Practice activities using QLearn and Deep QLearn on Gymnasium environments
+
+## To-Do:
+- Implement moving average on training log of total reward
--- utils/plots_cliffwalking.py
+++ utils/plots_cliffwalking.py
@@ -1,9 +1,10 @@
 import numpy as np
+import torch
 import matplotlib.pyplot as plt
 
 a = 'red'
 
-def plot_arrows(q_table):
+def plot_arrows_from_qtable(q_table):
     """"
     Cria um gráfico do tabuleiro em que cada posição possui setas proporcionais ao QValor de cada ação.
     """
@@ -40,5 +41,37 @@ def plot_arrows(q_table):
     plt.show()
     plt.savefig('arrowplot_cliffwalking.png')
 
-def plot_alpha():
-    pass
+def plot_arrows_from_qnet(q_net):
+    """"
+    Cria um gráfico do tabuleiro em que cada posição possui setas proporcionais ao Q-valor de cada ação.
+    """
+    board = np.ones((4, 12))
+    board[0, 1:11] = 0
+    plt.imshow(board, cmap='gray', origin='lower')
+    plt.grid()
+
+    for position in range(48):
+        if position in range(37, 47):
+            continue
+
+        x = position % 12
+        y = 3 - position // 12
+        
+        qvals = q_net(torch.tensor(position, dtype=torch.float32))
+        normalized_q_values = 0.4 * torch.softmax(qvals, dim=-1).detach().numpy()
+        
+        # cima
+        plt.arrow(x, y, 0, normalized_q_values[0], head_width=0.1, head_length=0.1, fc=a, ec=a)
+        # direita
+        plt.arrow(x, y, normalized_q_values[1], 0, head_width=0.1, head_length=0.1, fc=a, ec=a)
+        # baixo
+        plt.arrow(x, y, 0, -normalized_q_values[2], head_width=0.1, head_length=0.1, fc=a, ec=a)
+        # esquerda
+        plt.arrow(x, y, -normalized_q_values[3], 0, head_width=0.1, head_length=0.1, fc=a, ec=a)
+
+    # pintando o buraco de preto
+    plt.xticks(np.arange(13) - 0.5)
+    plt.yticks(np.arange(5) - 0.5)
+    plt.axis('scaled')
+    plt.show()
+    plt.savefig('arrowplot_cliffwalking.png')
